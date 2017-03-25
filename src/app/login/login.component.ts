8bf0cd53eadd0c55dcb959faf71658476cd8371c
zhongzhong0505 <zt_zhong@qq.com>
2017-03-25 10:50:41 +0800

    解决登录页面aot报错问题，都是小问题

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,28 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, ActivatedRouteSnapshot, RouterState, RouterStateSnapshot } from '@angular/router';
 import { User } from '../models/user-model';
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+	selector: 'login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-	public user:User = new User();
+	public user: User = new User();
 
-  	constructor(
-  		public router: Router,
-        public activatedRoute: ActivatedRoute
-  	) { 
+	constructor(
+		public router: Router,
+		public activatedRoute: ActivatedRoute
+	) {
 
-  	}
+	}
 
-  	ngOnInit() {
+	ngOnInit() {
 
-  	}
+	}
 
-  	login(){
-  		this.router.navigateByUrl("workspace");
-  	}
+	login() {
+		this.router.navigateByUrl("workspace");
+	}
+	forgetPwd() {
+		
+	}
 }
